5dfc4e561ab96fa29c54f394af849e1361941007
SuperWhw <wanghanwei7@163.com>
2020-12-14 23:22:08 +0800

    Change Territory and GameMap

--- src/Client/GameMap.java
+++ src/Client/GameMap.java
@@ -1,14 +1,41 @@
 package Client;
 
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GameMap {
     private HashSet<Territory> territorySet;
-    private Map<Territory, HashSet<Territory>> territoryRelation;
-    private int initUnits;
+    private final int initUnits;
 
-    public GameMap() {
+    public GameMap(HashSet<Territory> tS, int initUnits) {
+        this.territorySet = tS;
+        this.initUnits = initUnits;
+    }
 
+    public boolean checkReachable(Territory a, Territory b) {
+        String p_name = a.getOwner().getName();
+        if(!p_name.equals(b.getOwner().getName())) return false;
+        // BFS
+        HashSet<Territory> visited = new HashSet<>();
+        Queue<Territory> q = new LinkedList<Territory>();
+        q.offer(a);
+        while(!q.isEmpty()) {
+            Territory t = q.poll();
+            visited.add(t);
+            if (t.getName().equals(b.getName())) return true;
+            for(Territory nbT: t.getNeighbors()) {
+                if(!visited.contains(nbT) && nbT.getOwner().getName().equals(p_name)) {
+                    q.offer(nbT);
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkAttackable(Territory a, Territory b) {
+        // IS attackable when a,b has different owners and a & b are neighbors
+        return !a.getOwner().getName().equals(b.getOwner().getName()) && a.getNeighbors().contains(b);
     }
 }
--- src/Client/Territory.java
+++ src/Client/Territory.java
@@ -2,14 +2,18 @@ package Client;
 
 import Client.Player;
 
+import java.util.HashSet;
+
 public class Territory {
-    private String territoryName;
+    private final String territoryName;
     private Player territoryOwner;
+    private final HashSet<Territory> neighbors;
     private int territoryUnits;
 
-    public Territory(String name, Player owner, int units) {
+    public Territory(String name, Player owner, HashSet<Territory> neighbors, int units) {
         this.territoryName = name;
         this.territoryOwner = owner;
+        this.neighbors = neighbors;
         this.territoryUnits = units;
     }
 
@@ -19,6 +23,10 @@ public class Territory {
 
     public Player getOwner() {
         return territoryOwner;
+    }
+
+    public HashSet<Territory> getNeighbors() {
+        return neighbors;
     }
 
     public int getUnits() {
