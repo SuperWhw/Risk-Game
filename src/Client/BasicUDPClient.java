f013e0a74eed2969ab4512997ccec8e6b1eef701
chtang <chtang@redhat.com>
2020-12-14 17:24:46 +0800

    Add client folder

---
+++ src/Client/BasicUDPClient.java
@@ -0,0 +1,11 @@
+package Client;
+import jave.net.DatagramSocket;
+
+public class BasicUDPClient {
+    DatagramSocket ds;
+    Byte[] ReadBuffer;
+    Byte[] WriteBuffer;
+    BasicUDPClient() {
+
+    }
+}
