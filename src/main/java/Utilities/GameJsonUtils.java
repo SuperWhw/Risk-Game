01f77d278a8bfec6765b17271e7fb3ddea23530e
chtang <chtang@redhat.com>
2020-12-17 22:48:30 +0800

    Update GameJsonUtils.java

--- src/main/java/Utilities/GameJsonUtils.java
+++ src/main/java/Utilities/GameJsonUtils.java
@@ -1,114 +1,92 @@
 package Utilities;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonArray;
 import Client.*;
+import com.google.gson.*;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class GameJsonUtils {
-    private String str;
-    public void print() {
-        System.out.println(str);
-    }
-    public GameJsonUtils() {
-        this.str = "";
-    }
-    public GameJsonUtils(String str) {
-        this.str = str;
-    }
-    private JsonObject toObject(OrderBasic order) {
-        JsonObject object = new JsonObject();
-        object.addProperty("name", order.getPlayer().getName());
-        object.addProperty("orderType", order.getOrderType());
-        object.addProperty("fromT", order.getFromT().getName());
-        object.addProperty("toT", order.getToT().getName());
-        object.addProperty("units",order.getUnits());
-        return object;
-    }
-    private JsonObject toObject(Territory territory, boolean full) {
-        JsonObject object = new JsonObject();
-        object.addProperty("name", territory.getName());
-        object.addProperty("units",territory.getUnits());
-        object.addProperty("owner",territory.getOwner().getName());
-        if(full == true) {
-            object.add("neighbors", toNeighborsObject(territory.getNeighbors()));
+
+    class PlayerJsonAdaptor {
+        public String name;
+        public ArrayList<String> territories;
+        public PlayerJsonAdaptor(Player player) {
+            this.name = player.getName();
+            for(Territory territory : player.getTerritories()) {
+                this.territories.add(territory.getName());
+            }
         }
-        return object;
-    }
-    private JsonArray toNeighborsObject(HashSet<Territory> territorySet) {
-        JsonArray jsonArray = new JsonArray();
-        for(Territory territory : territorySet) {
-            JsonObject object = new JsonObject();
-            object.addProperty("name",territory.getName());
-            jsonArray.add(object);
+        private PlayerJsonAdaptor() { }
+        public void print() {
+            System.out.println("Player\nname : " + this.name + '\n' + "territories: " + this.territories);
         }
-        return jsonArray;
     }
 
-    private JsonArray toArray(HashSet<Territory> territorySet, boolean full) {
-        JsonArray jsonArray = new JsonArray();
-        for(Territory territory: territorySet) {
-            jsonArray.add(this.toObject(territory, full));
+    class TerritoryJsonAdaptor {
+        public String name;
+        public String owner;
+        public int units;
+        public ArrayList<String> neighbors;
+        public void print() {
+            System.out.println("Territory\nname : " + this.name + '\n' + "neighbors: " + this.neighbors + '\n'
+                    + "owner: " + this.owner + "\nunits: " + this.units);
         }
-        return jsonArray;
-    }
-
-    private JsonObject toObject(HashSet<Territory> territorySet, boolean full) {
-        JsonObject object = new JsonObject();
-        object.add("territorySet", this.toArray(territorySet,full));
-        return object;
-    }
-
-    private JsonObject toObject(GameMap gameMap) {
-        JsonObject object = new JsonObject();
-        object.add("territorySet",this.toObject(gameMap.getTerritorySet(),true));
-        object.addProperty("Map",gameMap.getInitUnits());
-        return object;
-    }
-
-    private JsonArray toArray(OrderBasic[] orderList) {
-        JsonArray array = new JsonArray();
-        for(OrderBasic order : orderList) {
-            array.add(this.toObject(order));
+        private TerritoryJsonAdaptor() { }
+        public TerritoryJsonAdaptor(Territory territory) {
+            this.neighbors = new ArrayList<String>();
+            this.owner = territory.getOwner().getName();
+            this.name = territory.getName();
+            this.units = territory.getUnits();
+            for(Territory Territory : territory.getNeighbors()) {
+                this.neighbors.add(Territory.getName());
+            }
         }
-        return array;
-    }
-    private JsonObject toObject(OrderBasic[] orderList) {
-        JsonObject object=  new JsonObject();
-        object.add("orderList", this.toArray(orderList));
-        return object;
     }
 
-    private JsonObject toObject(Player player) {
-        JsonObject object = new JsonObject();
-        object.addProperty("name",player.getName());
-        return object;
+    class GameMapJsonAdaptor {
+        public ArrayList<PlayerJsonAdaptor> players;
+        public ArrayList<TerritoryJsonAdaptor> territorySet;
+        public int initUnits;
+        public void print() {
+            System.out.println("GameMap:\nInitUnit: " + this.initUnits);
+            for(PlayerJsonAdaptor t : this.players) {
+                t.print();
+            }
+            for(TerritoryJsonAdaptor t : this.territorySet) {
+                t.print();
+            }
+        }
+        private GameMapJsonAdaptor() { }
+        public GameMapJsonAdaptor(GameMap gameMap, ArrayList<Player> players) {
+            this.players = new ArrayList<PlayerJsonAdaptor>();
+            this.territorySet = new ArrayList<TerritoryJsonAdaptor>();
+            this.initUnits = gameMap.getInitUnits();
+            for(Territory territory : gameMap.getTerritorySet()) {
+                this.territorySet.add(new TerritoryJsonAdaptor(territory));
+            }
+            for(Player player : players) {
+                this.players.add(new PlayerJsonAdaptor(player));
+            }
+        }
+    }
+    public void writeToJson(GameMap gameMap, ArrayList<Player> players) {
+
+        GameMapJsonAdaptor adaptor =  new GameMapJsonAdaptor(gameMap, players);
+        adaptor.print();
+
+    }
+    public void writeToJson(Player player) {
+        PlayerJsonAdaptor adaptor = new PlayerJsonAdaptor(player);
+        adaptor.print();
     }
 
-    public void toString(OrderBasic[] orderList) {
-        Gson gson = new Gson();
-        this.str = gson.toJson(this.toObject(orderList));
+    public void writeToJson(Territory territory) {
+        TerritoryJsonAdaptor adaptor = new TerritoryJsonAdaptor(territory);
+        adaptor.print();
     }
 
-    public void toString(GameMap gameMap) {
-        Gson gson = new Gson();
-        this.str = gson.toJson(this.toObject(gameMap));
-    }
-
-    public void toString(HashSet<Territory> territories) {
-        Gson gson = new Gson();
-        this.str = gson.toJson(this.toObject(territories,false));
-    }
-
-    public String getString() {
-        return this.str;
-    }
-    public static void main(String[] args) {
-        GameJsonUtils gju = new GameJsonUtils();
-
+    public static void main(String[] argv) {
         // create players
         Player p1 = new Player("p1");
         Player p2 = new Player("p2");
@@ -181,26 +159,20 @@ public class GameJsonUtils {
         AttackOrder a1 = new AttackOrder(p1, "attack", Midkemia, Scadrial, 10);
         AttackOrder a2 = new AttackOrder(p2, "attack", Scadrial, Hogwarts, 4);
 
-        OrderBasic[] orders = new OrderBasic[]{m1,m2,a1,a2};
+        ArrayList<OrderBasic> orders = new ArrayList<OrderBasic>();
+        orders.add(m1);
+        orders.add(m2);
+        orders.add(a1);
+        orders.add(a2);
 
         GameMap map = new GameMap();
         Territory[] territories = new Territory[]{Narnia,Midkemia,Oz,Elantris,Scadrial,Roshar,Gondor,Mordor,Hogwarts};
         map.buildMap(territories);
-        //print orderList
-        gju.toString(orders);
-        gju.print();
-
-        //print gameMap
-        gju.toString(map);
-        gju.print();
-        String gameMap = gju.getString();
-
-        //print territories
-        gju.toString(map.getTerritorySet());
-        gju.print();
-
-        FileIOBasics fiob = new FileIOBasics();
-        //fiob.writeFile("map.json",gameMap);
-        System.out.println(fiob.readJsonFile("a.json"));
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(p1);
+        players.add(p2);
+        players.add(p3);
+        GameJsonUtils gjU = new GameJsonUtils();
+        gjU.writeToJson(map, players);
     }
 }
