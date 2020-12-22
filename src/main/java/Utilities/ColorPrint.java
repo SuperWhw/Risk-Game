9b2b0bfd19b1eac095d5e6f75ade2b9985b3c7a1
ctang131 <ct265@duke.edu>
2020-12-22 16:19:08 +0800

    modify colorprint.java

--- src/main/java/Utilities/ColorPrint.java
+++ src/main/java/Utilities/ColorPrint.java
@@ -7,9 +7,12 @@ public class ColorPrint {
     public void printWithColor(String s, Color color) {
         System.out.println("\033[" + (90 + color.ordinal()) + ";1m" + s + "\033[0m");
     }
+    public String addColor(String s, Color color) {
+        return "\033[" + (90 + color.ordinal()) + ";1m" + s + "\033[0m";
+    }
     public static void main(String[] args) {
         ColorPrint print = new ColorPrint();
-        print.printWithColor("hello world", Color.GREEN);
+        print.printWithColor("hello world", Color.SKYBLUE);
     }
 
 }
