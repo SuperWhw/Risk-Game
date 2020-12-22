10a778b3800328f2bffa9ba01b0ad9b8c51666b2
ctang131 <ct265@duke.edu>
2020-12-22 21:06:05 +0800

    update controller

--- src/main/java/Server/GameServerController.java
+++ src/main/java/Server/GameServerController.java
@@ -5,43 +5,66 @@ import Shared.*;
 import Utilities.FileIOBasics;
 import Utilities.GameJsonUtils;
 
-import java.awt.*;
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import Utilities.*;
 import java.io.IOException;
 import java.lang.*;
 import java.util.ArrayList;
 
 public class GameServerController {
+    BasicTCPServer server;
+    ColorPrint printer;
+    GameMap gameMap;
+    FileIOBasics fileIO;
+    GameJsonUtils jsonUtils;
+    GameClientViewer viewer;
+    GameServerController(int port, int playerNum) {
+        printer = new ColorPrint();
+        fileIO = new FileIOBasics();
+        jsonUtils = new GameJsonUtils();
+        viewer = new GameClientViewer();
+        try {
+            server = new BasicTCPServer(port, playerNum);
+        }
+        catch (IOException e) {
+            printer.printWithColor("TCP Server build failure, please check your network",
+                    ColorPrint.Color.RED);
+        }
+    }
 
-    public static void main(String[] args) throws IOException {
-        var server = new BasicTCPServer(6666,3);
-        server.CreateSockets();
+    public boolean isGameDone(){
+        for(var player : gameMap.getPlayerMap().values()) {
+            if(player.getTerritories().size() == gameMap.getTerritoryMap().size()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void InitializeMap() {
         server.sendMessage("Hello this is your host, please input your name:\n");
-        var players = server.receiveMessage();
-        server.end();
 
+        // create n players
+        var players = server.receiveMessage();
         var playerList = new ArrayList<Player>();
         for(var player : players) {
             playerList.add(new Player(player));
         }
 
-        var fileIO = new FileIOBasics();
+        // read and load map.json
         var MapStr = fileIO.readJsonFile("map.json");
+        gameMap = jsonUtils.readJsonToGameMap(MapStr, players);
 
-        var jsonUtil = new GameJsonUtils();
-        var gameMap = jsonUtil.readJsonToGameMap(MapStr, players);
+        // printMap
 
         var viewer = new GameClientViewer();
         for(var player : gameMap.getPlayerMap().values()) {
             viewer.printMap(gameMap, player, "order");
         }
 
-        var MapStrAfterOrders = jsonUtil.writeMapToJson(gameMap,null);
-        System.out.println(MapStrAfterOrders);
-
+    }
+    public static void main(String[] args) throws IOException {
+        var control = new GameServerController(6666,3);
+        control.InitializeMap();
 
     }
 
